f1680f2ccfbd86987f39153f479ac0c79f4364ac
Paul Bückhard <104130878+PaulBueckhard@users.noreply.github.com>
2022-05-01 16:47:03 +0200

    Add files via upload

---
+++ app/main/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template, Blueprint
+from flask_login import login_required
+
+main = Blueprint("main", __name__)
+
+@main.route("/")
+@main.route("/home")
+@login_required
+def home():
+    return render_template("main.html")
